3f17baac1937ea6d1e21909f4b0ca9d4dd50294a
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2024-01-17 13:22:12 +0100

    Add CORS headers to sales_controller.php

--- sales_controller.php
+++ sales_controller.php
@@ -1,5 +1,14 @@
 <?php
 
+// allow access from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+  // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+  // you want to allow, and if so:
+  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+  header('Access-Control-Allow-Credentials: true');
+  header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
 $data_inizio = $_GET["data_inizio"];
 $data_fine = $_GET["data_fine"];
 $range_date = $_GET["range_date"];
